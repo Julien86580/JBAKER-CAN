85395d599e3afabd539159a6b2b1310db7420d77
Nathan86Julien86fr <145715320+Nathan86Julien86fr@users.noreply.github.com>
2023-11-29 18:29:48 +0100

    Delete Server-Fivem directory

--- Server-Fivem/client.lua
+++
@@ -1,7 +0,0 @@
--- client.lua
-
-local FindSpawnPointInDirection(x1, y1, z1, x2, y2, z2, distance, spawnPoint)
-local FindSpawnPointIn = FindSpawnPointInDirection -- Find spawn point in GetWindDirection()
-
-function DeleteAllTrains()
-    

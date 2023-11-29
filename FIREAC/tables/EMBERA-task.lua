b4d112c3ceb665347a78a36f0f0a2b1b4328d146
Nathan86Julien86fr <145715320+Nathan86Julien86fr@users.noreply.github.com>
2023-11-29 18:30:02 +0100

    Delete FIREAC directory

--- FIREAC/tables/EMBERA-task.lua
+++
@@ -1,9 +0,0 @@
--- Do not touch these codes as much as possible (touch them if you are familiar and confident)
-
-Tasks = {
-	100, -- CTaskWanderingScenario
-	101, -- CTaskWanderingInRadiusScenario
-	151, -- CTaskCarDriveWander
-	221, -- CTaskWander
-	222, -- CTaskWanderInArea
-}
